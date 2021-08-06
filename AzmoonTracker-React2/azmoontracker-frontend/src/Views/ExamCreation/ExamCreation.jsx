2aed7b55afbaf52ea079bc6d4a61facb2ee5a771
X.V <academic122@gmail.com>
2021-08-07 00:14:17 +0430

    updating exam creation form UI

--- AzmoonTracker-React2/azmoontracker-frontend/src/Views/ExamCreation/ExamCreation.jsx
+++ AzmoonTracker-React2/azmoontracker-frontend/src/Views/ExamCreation/ExamCreation.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../../styles/ExamCreation.css'
 import QuestionCreation from './QuestionCreation'
 import {useForm} from "react-hook-form"
@@ -6,7 +6,26 @@ import {useForm} from "react-hook-form"
 export default function ExamCreation() {
     
     const {register, handleSubmit, formState: {errors}} = useForm() ;
-
+    
+    const [Exam, setExam] = useState({
+            examId: "",
+            examSearchId: "",
+            examName: "",
+            className: "",
+            isPublic: true,
+            isFinished: false,
+            questionNum: 1,
+            startTime: "",
+            endTime: "",
+            questions: [
+              {
+                questionTypeId: 1,
+                questionNum: 1,
+                questionDescription: ""
+              }
+            ]
+        });
+        
     const onSubmit = (data) =>
     {
         console.log(data);
@@ -17,20 +36,19 @@ export default function ExamCreation() {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div>
                     <label>exam title:</label>
-                    <input type="text" id="examTitle" placeholder="enter exam title" name="examTitle"
-                    {...register("examTitle", {required: {value:true, message:"PASSWORD is required."}, 
-                    minLength: {value:8, message:"PASSWORD is too short."}})}/>
+                    <input type="text" placeholder="enter exam title" name="examTitle"
+                    {...register("examTitle")}/>
                 </div>
                 {errors.examTitle && <p>{errors.examTitle.message}</p>}
                 <br/>
                 <div>
                     <label>date:</label>
-                    <input type="date" name="examDate" {...register("examDate", {required: true})}/>
+                    <input type="date" name="examDate" {...register("examDate")}/>
                 </div>
                 <br/>
                 <div>
                     <label>time:</label>
-                    <input type="time" name="examTime" {...register("examTime", {required: true})}/>
+                    <input type="time" name="examTime" {...register("examTime")}/>
                 </div>
                 <br/>
                 <div>
@@ -44,13 +62,39 @@ export default function ExamCreation() {
                     <input type="file" name="examImage"/>
                 </div>
                 <br/>
-
                 <button>submit</button>
+
+                <div className="add_question"> + </div>
+                {
+                Exam.questions.map(question => 
+                //questionTypeId: 1,
+                //questionNum: 1,
+                //questionDescription: ""
+                <div className="exam_question">
+                    <div className="collapse_question"> {question.questionNum} </div>
+                    <div className="remove_question">remove</div>
+                    <div>
+                        <label>question type:</label>
+                        <select name="questionType">
+                            <option selected={question.questionTypeId==1}>Descriptive</option>
+                            <option selected={question.questionTypeId==2}>Multi-choice</option>
+                            <option selected={question.questionTypeId==3}>File upload</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div>
+                        <label>enter question:</label>
+                        <input type="text" placeholder="" name="questionDescription" 
+                        value={question.questionDescription}/>
+                    </div>
+                </div>
+                )}
             </form>
             <br/>
 
-            <div className="add_question"> +</div>
-            <QuestionCreation />
+            {//<div className="add_question"> +</div>
+            //<QuestionCreation />
+            }
         </div>
     )
 
