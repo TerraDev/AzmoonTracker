be53ad3d48556dcad7d8a92ed41be04f09d714de
X.V <academic122@gmail.com>
2021-08-07 23:47:29 +0430

    updating createExam UI

--- AzmoonTracker-React2/azmoontracker-frontend/src/Views/ExamCreation/ExamCreation.jsx
+++ AzmoonTracker-React2/azmoontracker-frontend/src/Views/ExamCreation/ExamCreation.jsx
@@ -7,27 +7,34 @@ export default function ExamCreation() {
     
     const {register, handleSubmit, formState: {errors}} = useForm() ;
     
-    const [Exam, setExam] = useState({
-            examId: "",
-            examSearchId: "",
-            examName: "",
-            className: "",
-            isPublic: true,
-            isFinished: false,
-            questionNum: 1,
-            startTime: "",
-            endTime: "",
-            questions: [
-              {
-                questionTypeId: 1,
-                questionNum: 1,
-                questionDescription: ""
-              }
-            ]
-        });
+    const blankQuestion = {
+        questionTypeId: 1,
+        questionNum: 1,
+        questionDescription: ""
+    }
+
+    const [Questions, setQuestions] = useState([
+            //examId: "",
+            //examSearchId: "",
+            //examName: "",
+            //className: "",
+            //isPublic: true,
+            //isFinished: false,
+            //questionNum: 1,
+            //startTime: "",
+            //endTime: "",
+            blankQuestion   //or ...blankQuestion
+    ]);
         
+    const addQuestion= () => {
+        const newBlankQuestion = {...blankQuestion, questionNum:Questions.length+1}
+
+        setQuestions([...Questions, newBlankQuestion])
+    }
+
     const onSubmit = (data) =>
     {
+        console.log(Questions);
         console.log(data);
     }
 
@@ -53,7 +60,7 @@ export default function ExamCreation() {
                 <br/>
                 <div>
                     <label>class name:</label>
-                    <input type="text" {...register("examClass", {required: true})}
+                    <input type="text" {...register("examClass")}
                     placeholder="eg: school year 9 physics,UCLA mechanical engineering,..." />
                 </div>
                 <br/>
@@ -64,31 +71,33 @@ export default function ExamCreation() {
                 <br/>
                 <button>submit</button>
 
-                <div className="add_question"> + </div>
+                <div className="add_question" onClick={addQuestion}> + </div>
+                <div className="ReverseList">
                 {
-                Exam.questions.map(question => 
-                //questionTypeId: 1,
-                //questionNum: 1,
-                //questionDescription: ""
-                <div className="exam_question">
-                    <div className="collapse_question"> {question.questionNum} </div>
-                    <div className="remove_question">remove</div>
-                    <div>
-                        <label>question type:</label>
-                        <select name="questionType">
-                            <option selected={question.questionTypeId==1}>Descriptive</option>
-                            <option selected={question.questionTypeId==2}>Multi-choice</option>
-                            <option selected={question.questionTypeId==3}>File upload</option>
-                        </select>
+                Questions.map(Question => 
+                    //questionTypeId: 1,
+                    //questionNum: 1,
+                    //questionDescription: ""
+                    <div className="exam_question">
+                        <div className="collapse_question"> {Question.questionNum} </div>
+                        <div className="remove_question">remove</div>
+                        <div>
+                            <label>question type:</label>
+                            <select name="questionType">
+                                <option selected={Question.questionTypeId==1}>Descriptive</option>
+                                <option selected={Question.questionTypeId==2}>Multi-choice</option>
+                                <option selected={Question.questionTypeId==3}>File upload</option>
+                            </select>
+                        </div>
+                        <br/>
+                        <div>
+                            <label>enter question:</label>
+                            <input type="text" placeholder="" name="questionDescription" 
+                            value={Question.questionDescription}/>
+                        </div>
                     </div>
-                    <br/>
-                    <div>
-                        <label>enter question:</label>
-                        <input type="text" placeholder="" name="questionDescription" 
-                        value={question.questionDescription}/>
-                    </div>
-                </div>
                 )}
+                </div>
             </form>
             <br/>
 
